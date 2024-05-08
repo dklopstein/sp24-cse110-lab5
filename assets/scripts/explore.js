2ded1c03286971b4ea9f0397e47656484c47ac88
dklopstein <dklopstein@ucsd.edu>
2024-05-07 21:46:17 -0700

    explore.js updated

--- assets/scripts/explore.js
+++ assets/scripts/explore.js
@@ -3,5 +3,64 @@
 window.addEventListener('DOMContentLoaded', init);
 
 function init() {
-  // TODO
+  // https://developer.mozilla.org/en-US/docs/Web/API/SpeechSynthesis
+  // populate voice options
+  const synth = window.speechSynthesis;
+
+  const voiceSelect = document.querySelector("select");
+
+  let voices = [];
+
+  function populateVoiceList() {
+    voices = synth.getVoices();
+
+    for (let i = 0; i < voices.length; i++) {
+      const option = document.createElement("option");
+      option.textContent = `${voices[i].name} (${voices[i].lang})`;
+
+      if (voices[i].default) {
+        option.textContent += " — DEFAULT";
+      }
+
+      option.setAttribute("data-lang", voices[i].lang);
+      option.setAttribute("data-name", voices[i].name);
+      voiceSelect.appendChild(option);
+    }
+  }
+
+  populateVoiceList();
+  if (speechSynthesis.onvoiceschanged !== undefined) {
+    speechSynthesis.onvoiceschanged = populateVoiceList;
+  }
+
+  // button that starts the voice
+  const talkBtn = document.querySelector('button');
+  const inputText = document.querySelector('textarea');
+  const smileyFace = document.querySelector('img');
+  talkBtn.addEventListener('click', function() {
+    if (voiceSelect.selectedOptions[0].getAttribute('value') == 'select') {
+      return;
+    }
+    const utterThis = new SpeechSynthesisUtterance(inputText.value);
+    const selectedOption =
+      voiceSelect.selectedOptions[0].getAttribute("data-name");
+    for (let i = 0; i < voices.length; i++) {
+      if (voices[i].name === selectedOption) {
+        utterThis.voice = voices[i];
+      }
+    }
+
+    // change the smiley face to open when speaking
+    utterThis.onstart = function() {
+      smileyFace.src = "assets/images/smiling-open.png";
+    };
+
+    // change the smiley face to closed when done speaking
+    utterThis.onend = function() {
+      // Change the face image back to smiling
+      smileyFace.src = "assets/images/smiling.png";
+    };
+    
+    synth.speak(utterThis);
+  });
 }
