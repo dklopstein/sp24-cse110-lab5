b7f60d6ae7c76a0de8b1d1d35e3bca6eec20ccf9
dklopstein <dklopstein@ucsd.edu>
2024-05-07 22:23:16 -0700

    finished unit tests

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 # Lab 5 - Starter
 
 Derek Klopstein
+
+Part 3:
+
+1. Probably not. One con of unit testing is that we cannot test how the individual components interact with each other. So in this case I don't think we're able to run a unit test between two different users, only individual things as a single user.
+
+---
+
+2. Yes, if possible. While it may be easy to test that right away, changing a different feature in the future could cause it to break. Having a unit test to ensure this feature always works would be beneficial.
--- __tests__/unit.test.js
+++ __tests__/unit.test.js
@@ -8,4 +8,87 @@ import {
   isHexColor,
 } from '../code-to-unit-test/unit-test-me';
 
-// TODO - Part 2
+// isPhoneNumber
+test('valid phone number', () => {
+  expect(isPhoneNumber('123-456-7890')).toBe(true);
+});
+
+test('valid long phone number', () => {
+  expect(isPhoneNumber('123-123-123-1234')).toBe(true);
+});
+
+test('invalid phone number short', () => {
+  expect(isPhoneNumber('911')).toBe(false);
+});
+
+test('invalid phone number with letters', () => {
+  expect(isPhoneNumber('abc')).toBe(false);
+});
+
+// isEmail
+test('valid email', () => {
+  expect(isEmail('test@gmail.com')).toBe(true);
+});
+
+test('valid email with numbers', () => {
+  expect(isEmail('1234@aol.co')).toBe(true);
+});
+
+test('invalid email', () => {
+  expect(isEmail('test@aol.comm')).toBe(false);
+});
+
+test('invalid email missing @', () => {
+  expect(isEmail('testaol.comm')).toBe(false);
+});
+
+// isStrongPassword
+test('valid strong password', () => {
+  expect(isStrongPassword('abcd')).toBe(true);
+});
+
+test('valid strong password wtih numbers', () => {
+  expect(isStrongPassword('abcd1234')).toBe(true);
+});
+
+test('invalid strong password too short', () => {
+  expect(isStrongPassword('ab')).toBe(false);
+});
+
+test('invalid strong password too long', () => {
+  expect(isStrongPassword('abcdefghijklmnopqrstuvwxyz')).toBe(false);
+});
+
+// isDate
+test('valid date single digit format', () => {
+  expect(isDate('1/1/2000')).toBe(true);
+});
+
+test('valid date double digit format', () => {
+  expect(isDate('01/01/2000')).toBe(true);
+});
+
+test('invalid date letters', () => {
+  expect(isDate('a1/01/2000')).toBe(false);
+});
+
+test('invalid date wrong format', () => {
+  expect(isDate('01/01/00')).toBe(false);
+});
+
+// isHexColor
+test('valid hex color length 3', () => {
+  expect(isHexColor('#000')).toBe(true);
+});
+
+test('valid hex color length 6', () => {
+  expect(isHexColor('#000000')).toBe(true);
+});
+
+test('invalid hex color too short', () => {
+  expect(isHexColor('00')).toBe(false);
+});
+
+test('invalid hex color too long', () => {
+  expect(isHexColor('#abc0323')).toBe(false);
+});
